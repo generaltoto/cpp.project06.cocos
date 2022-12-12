04e053d1f1012890079d5bca50e4e30b70bb576f
Thomas <tdelorme@gaming.tech>
2022-12-12 09:41:21 +0100

    🌟 changed window to fullscreen

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -73,7 +73,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("cpp.project06.cocos", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithFullScreen("Lemmings");
 #else
         glview = GLViewImpl::create("cpp.project06.cocos");
 #endif
