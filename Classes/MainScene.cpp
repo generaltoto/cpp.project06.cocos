2440baab5a9f786d8c0e740b344feb9fa2c6828b
Thomas <tdelorme@gaming.tech>
2022-12-13 11:06:20 +0100

    🌟 displaying sprite with collisions on screen

--- Classes/MainScene.cpp
+++ Classes/MainScene.cpp
@@ -2,27 +2,69 @@
 
 USING_NS_CC;
 
+MainScene::MainScene()
+{
+    this->spLemming = nullptr;
+}
+
 Scene* MainScene::createScene()
 {
-    return MainScene::create();
+    const auto _sceneWithPhysics = cocos2d::Scene::createWithPhysics();
+    _sceneWithPhysics->getPhysicsWorld()->setDebugDrawMask(cocos2d::PhysicsWorld::DEBUGDRAW_ALL);
+
+    const auto _layerWithPhysics = MainScene::create();
+    _sceneWithPhysics->addChild(_layerWithPhysics);
+
+    return _sceneWithPhysics;
 }
 
-// Print useful error message instead of segfaulting when files are not there.
-static void problemLoading(const char* filename)
-{
-    printf("Error while loading: %s\n", filename);
-    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
-}
-
-// on "init" you need to initialize your instance
 bool MainScene::init()
 {
-    if ( !Scene::init() ) return false;
+    if ( !Layer::init() ) return false;
 
-    const auto visibleSize = Director::getInstance()->getVisibleSize();
-    const Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
-
+    this->visibleSize = Director::getInstance()->getVisibleSize();
 
     return true;
+}
+
+void MainScene::onEnter()
+{
+    cocos2d::Layer::onEnter();
+	this->addLemming(visibleSize.width / 2, visibleSize.height / 2);
+	this->addWindowsEdgesCollider();
+}
+
+void MainScene::update(float delta)
+{
+    Node::update(delta);
+
+    const float _curPosX = this->spLemming->getPositionX();
+    this->spLemming->setPositionX(_curPosX + 10 * delta);
+}
+
+void MainScene::addWindowsEdgesCollider()
+{
+    const auto body = cocos2d::PhysicsBody::createEdgeBox(visibleSize, cocos2d::PHYSICSBODY_MATERIAL_DEFAULT,1);
+
+    const auto edgeShape = cocos2d::Node::create();
+    edgeShape->setPhysicsBody(body);
+    edgeShape->setPosition(visibleSize.width / 2, visibleSize.height / 1.7f);
+    this->addChild(edgeShape);
+}
+
+void MainScene::addLemming(float positionX, float positionY)
+{
+    this->spLemming = Sprite::create("HelloWorld.png");
+    if (this->spLemming == nullptr) throw ERROR_BAD_PATHNAME;
+
+    this->spLemming->setPosition(positionX, positionY);
+    this->addChild(this->spLemming);
+
+    const auto _lemmingPhysicBody = cocos2d::PhysicsBody::createBox(
+        cocos2d::Size(this->spLemming->getContentSize()),
+        cocos2d::PhysicsMaterial(0.1f,1.0f,0.0f)
+    );
+    _lemmingPhysicBody->setDynamic(true);
+    _lemmingPhysicBody->setGravityEnable(true);
+	this->spLemming->addComponent(_lemmingPhysicBody);
 }
--- Classes/MainScene.h
+++ Classes/MainScene.h
@@ -2,15 +2,50 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "Box2D/Box2D.h"
 
-class MainScene : public cocos2d::Scene
+class MainScene : public cocos2d::Layer
 {
 public:
-    static cocos2d::Scene* createScene();
+	MainScene();
 
-    bool init() override;
+	/**
+	 * \brief Creates the scene
+	 * \return The scene created with physics
+	 */
+	static cocos2d::Scene* createScene();
 
-    CREATE_FUNC(MainScene);
+	/**
+	 * \brief Initiates all members
+	 * \return true if succeeded
+	 */
+	bool init() override;
+
+	/**
+	 * \brief Loads everything into the scene
+	 */
+	void onEnter() override;
+
+	void update(float delta) override;
+
+    CREATE_FUNC(MainScene)
+
+private:
+    cocos2d::Sprite *spLemming;
+
+    cocos2d::Size visibleSize;
+
+	/**
+	 * \brief Creates a collision box with the window borders
+	 */
+	void addWindowsEdgesCollider();
+
+	/**
+	 * \brief Creates a Lemming with physics.
+	 * \param positionX X Position of the Lemming (cannot be greater than the ScreenWidth)
+	 * \param positionY Y Position of the Lemming (cannot be greater than the ScreenHeight)
+	 */
+	void addLemming(float positionX, float positionY);
 };
 
 #endif // __HELLOWORLD_SCENE_H__
